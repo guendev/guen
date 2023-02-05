a51201940763c8a14f86835c3065f8c76d45e64d
yuan <nguyenshort@gmail.com>
2023-02-05 23:16:33 +0700

    auto backup support

---
+++ pages/admin/posts/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <blog-wrapper>
+    <client-only>
+      <admin-posts-list />
+    </client-only>
+  </blog-wrapper>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped></style>
