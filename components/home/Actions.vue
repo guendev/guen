c0dae57a22fdf8f87c74b999368eaba17e0f4650
yuan <nguyenshort@gmail.com>
2023-02-07 23:01:39 +0700

    add editor render view

--- components/home/Actions.vue
+++ components/home/Actions.vue
@@ -1,22 +1,20 @@
 <template>
   <div class="flex items-center">
-
-    <button class="bg-gradient-to-r from-rose-500 to-rose-600 text-white lg:px-5 lg:py-2.5 px-4 py-2 flex justify-center items-center shadow-default shadow-rose-300">
+    <a
+        target="_blank"
+        href="mailto:heloo@guen.dev"
+        class="bg-gradient-to-r from-rose-500 to-rose-600 text-white lg:px-5 lg:py-2.5 px-4 py-2 flex justify-center items-center shadow-default shadow-rose-300"
+    >
       <span class="lg:text-[15px] text-[14px] font-semibold">Hire Me</span>
-    </button>
+    </a>
 
-    <button class="ml-7 font-bold text-gray-800" @click="openProjects">
+    <a target="_blank" href="https://github.com/nguyenshort" class="ml-7 font-bold text-gray-800">
       Projects
       <Icon class="text-[18px]" name="material-symbols:arrow-outward-rounded" />
-    </button>
-
+    </a>
   </div>
 </template>
 
-<script lang="ts" setup>
-const openProjects = () => {
-  window.open('https://github.com/nguyenshort', '_blank')
-}
-</script>
+<script lang="ts" setup></script>
 
 <style scoped></style>
